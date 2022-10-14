8ef422a2f80631fef28e6bbdde944820d2178934
ECODELI\oswaldo.herrera <oswaldo.herrera.mx@gmail.com>
2022-10-14 14:52:40 -0500

    finseccion3 - diseño

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.scss']
+})
+export class NopagefoundComponent  {
+  year = new Date().getFullYear();
+
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
+
+}
